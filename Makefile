f9263ba2268ebbf6a2492fe431aeb2ad40863249
Jason A. Wilson <jasonaaronwilson@gmail.com>
2023-02-14 03:38:44 -0500

    Make some progress showing the basic system calls work.

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ SRC=system-call-numbers.c linux-x86-64.S main.c system-call.h
 
 main: ${SRC}
 	gcc system-call-numbers.c -o system-call-numbers
-	gcc -nostdlib linux-x86-64.S main.c -o main
+	gcc -g -nostdlib linux-x86-64.S main.c -o main
 
 clean:
 	rm -f *~ a.out ./system-call-numbers ./main
--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include "system-call.h"
 
+#define PATH_MAX 4096
+
+void show_newline() {
+  syscall(SYS_write, 1, "\n", 1);
+}
+
+//
 // This is just a simple definition of strlen (though it accepts NULL
 // as an argument because why not).
+//
 long c_string_length(char *str) {
   if (str == 0) {
     return 0;
@@ -14,6 +22,17 @@ long c_string_length(char *str) {
   syscall(SYS_exit, 1);
 }
 
+//
+// Print a string to stdout followed by a newline.
+// 
+void show_c_string(char *str) {
+  syscall(SYS_write, 1, str, c_string_length(str));
+  show_newline();
+}
+
+//
+// Reverse a C style (NUL terminated) string.
+//
 void reverse_c_string(char *str) {
   long len = c_string_length(str);
   if (len > 1) {
@@ -32,6 +51,10 @@ void reverse_c_string(char *str) {
   }
 }
 
+//
+// This is like sprintf(buffer, "%d", number) except we probably don't
+// handle negative numbers correctly.
+//
 void long_to_c_string(char *buffer, long number) {
   int offset = 0;
  loop:
@@ -50,24 +73,36 @@ void long_to_c_string(char *buffer, long number) {
   }
 }
 
-#define PATH_MAX 4096
+//
+// Print a non-negative number to stdout with a newline.
+//
+void show_number(long number) {
+  long result = 0;
+  char buffer[PATH_MAX];
+  long_to_c_string(buffer, number);
+  show_c_string(buffer);
+}
 
+//
+// Print the current working directory to stdout with a newline.
+//
 void show_current_working_directory() {
   long result = 0;
   char buffer[PATH_MAX];
   result = syscall(SYS_getcwd, buffer, PATH_MAX);
-  result = syscall(SYS_write, 1, buffer, c_string_length(buffer));
-  result = syscall(SYS_write, 1, "\n", 1);
+  show_c_string(buffer);
 }
 
 int main(int argc, char **argv) {
   long result = 0;
-  char buffer[PATH_MAX];
+
+  for (int i = 0; i < argc; i++) {
+    show_number(i);
+    show_c_string(argv[i]);
+  }
 
   // Test our long to string
-  long_to_c_string(buffer, 9190919);
-  result = syscall(SYS_write, 1, buffer, c_string_length(buffer));
-  result = syscall(SYS_write, 1, "\n", 1);
+  // long_to_c_string(buffer, 9190919);
 
   show_current_working_directory();
   result = syscall(SYS_chdir, "..");
