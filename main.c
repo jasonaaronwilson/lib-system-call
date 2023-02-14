9bffa28a84cb742fd0a75c773d095ab07806a782
Jason A. Wilson <jasonaaronwilson@gmail.com>
2023-02-14 01:28:50 -0500

    Test SYS_getcwd.

--- main.c
+++ main.c
@@ -1,13 +1,38 @@
 #include "system-call.h"
 
+long c_string_length(char *str) {
+  if (str == 0) {
+    return 0;
+  }
+  for (long i = 0; 1; i++) {
+    if (str[i] == 0) {
+      return i;
+    }
+  }
+  syscall(SYS_exit, 1);
+}
+
+#define PATH_MAX 4096
+
 int main(int argc, char **argv) {
 
-  // fprintf(stdout, "SYS_write = %d\n", SYS_write);
-  // fprintf(stdout, "SYS_exit = %d\n", SYS_exit);
+  char buffer[PATH_MAX];
 
-     long errror_1 = syscall(SYS_write, 1, "Hello World!\n", 13);
-     long error = syscall(SYS_exit, 42);
-     return 0;
+  long result = 0;
+
+  result = syscall(SYS_getcwd, buffer, PATH_MAX);
+
+  result = syscall(SYS_write, 1, buffer, c_string_length(buffer));
+  result = syscall(SYS_write, 1, "\n", 1);
+
+  // This doesn't actually appear to work...
+  result = syscall(SYS_chdir, 1, "..");
+
+  result = syscall(SYS_write, 1, "Hello World!\n", 13);
+
+  result = syscall(SYS_exit, 42);
+
+  return 0;
 }
 
 // Tell the compiler incoming stack alignment is not RSP%16==8 or ESP%16==12
